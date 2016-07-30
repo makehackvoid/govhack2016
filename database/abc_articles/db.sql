9b34ff8bea748f78f338653443bf362d30a47e0a
Max Bainrot <mbainrot@users.noreply.github.com>
2016-07-30 16:59:13 +1000

    - relocated everything to a more logical location - added worklog_txt which explains what I did to clean up the dataset (by killing the bad rows)
    import_data.py - not being implemented, simple queries do the job better and faster
    
    convert_article_timestamps.py - This basically takes the YYYY-MM-DD hh:mm:ss and converts it to milliseconds since EPOCH, on a reasonably meaty box it takes around 2 minutes and 20 seconds to do it's thing

---
+++ database/abc_articles/db.sql
@@ -0,0 +1,108 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: article_meta; Type: TABLE; Schema: public; Owner: max; Tablespace:
+--
+
+CREATE TABLE article_meta (
+    contentid integer,
+    contenttype text,
+    contenttitletag text,
+    contentheadline text,
+    contentkeywords text,
+    contentlatitude text,
+    contentlongitude text,
+    contentnewsfromothersites text,
+    contentcomments text,
+    contentlength text,
+    contentparagraphs text,
+    contentwords text,
+    contentduration text,
+    contentvideos text,
+    contentaudio text,
+    contentphotos text,
+    contentgalleries text,
+    contentmaps text,
+    contentrelatedstories text,
+    contentfirstpublished bigint,
+    contentdescription text,
+    contenttopics text
+);
+
+
+ALTER TABLE article_meta OWNER TO max;
+
+--
+-- Name: article_meta_raw; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
+--
+
+CREATE TABLE article_meta_raw (
+    contentid text,
+    contenttype text,
+    contenttitletag text,
+    contentheadline text,
+    contentkeywords text,
+    contentlatitude text,
+    contentlongitude text,
+    contentnewsfromothersites text,
+    contentcomments text,
+    contentlength text,
+    contentparagraphs text,
+    contentwords text,
+    contentduration text,
+    contentvideos text,
+    contentaudio text,
+    contentphotos text,
+    contentgalleries text,
+    contentmaps text,
+    contentrelatedstories text,
+    contentfirstpublished text,
+    contentdescription text,
+    contenttopics text
+);
+
+
+ALTER TABLE article_meta_raw OWNER TO postgres;
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
